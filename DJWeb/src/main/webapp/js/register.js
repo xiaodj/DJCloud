9490f3c1b07969c543d2e38ab62c57fbfe8132c1
xiaodj <xiaodj20140808@163.com>
2018-08-25 15:43:16 +0800

    整合前端

---
+++ DJWeb/src/main/webapp/js/register.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaodj on 2018/7/16.
+ */
+layui.define(['layer','form'],function (exports) {
+    var layer = layui.layer;
+    var form = layui.form;
+    //var element = layui.element;
+    exports('register', {});
+});
