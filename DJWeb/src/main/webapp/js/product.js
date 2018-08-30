8e82a0f89d0e8336bd31207952c310dddb795f4e
dengjiang <dengjiang@guahao.com>
2018-08-30 18:16:22 +0800

    添加产品管理模块

--- DJWeb/src/main/webapp/js/product.js
+++ DJWeb/src/main/webapp/js/product.js
@@ -1,26 +1,27 @@
 /**
  * Created by xiaodj on 2018/7/18.
  */
-layui.define(['layer','form','element'],function (exports) {
+layui.define(['layer','form'],function (exports) {
     var layer = layui.layer;
     var form = layui.form;
-    var element = layui.element;
+    //var element = layui.element;
+    var $ = layui.$;
     
-    form.on('select(DAP)', function (data) {
-        if (data.value == 0){
+    form.on('select(accprot)', function (data) {
+        if (data.value == "HTTP"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>HTTP 协议基本功能介绍：</b>";
             var vp = "<p>1、上报传感器数据点</p>";
             eid.innerHTML = vb+vp;
             //layer.msg("HTTP");
-        }else if (data.value == 1){
+        }else if (data.value == "EDP"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>EDP 协议基本功能介绍：</b>";
             var vp = "<p>1、上报传感器数据点</p>";
             vp = vp + "<p>2、接收实时命令或离线命令</p>";
             eid.innerHTML = vb+vp;
             //layer.msg("EDP");
-        }else if (data.value == 2){
+        }else if (data.value == "MQTT"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>MQTT 协议基本功能介绍：</b>";
             var vp = "<p>1、上报传感器数据点</p>";
@@ -28,21 +29,21 @@ layui.define(['layer','form','element'],function (exports) {
             vp = vp + "<p>3、接收实时消息或离线消息</p>";
             eid.innerHTML = vb+vp;
             //layer.msg("MQTT");
-        }else if (data.value == 3){
+        }else if (data.value == "Modbus"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>Modbus 协议基本功能介绍：</b>";
             var vp = "<p>1、自定义采集命令与采集周期</p>";
             vp = vp + "<p>2、接收实时命令或离线命令</p>";
             eid.innerHTML = vb+vp;
             //layer.msg("Modbus");
-        }else if (data.value == 4){
+        }else if (data.value == "JT/T808"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>JT/T808 协议基本功能介绍：</b>";
             var vp = "<p>1、上报传感器数据点</p>";
             vp = vp + "<p>2、接收实时命令或离线命令</p>"
             eid.innerHTML = vb+vp;
             //layer.msg("JT/T808");
-        }else if (data.value == 5){
+        }else if (data.value == "TCP透传"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>TCP透传基本功能介绍：</b>";
             var vp = "<p>1、数据透传</p>";
@@ -50,7 +51,7 @@ layui.define(['layer','form','element'],function (exports) {
             vp = vp + "<p>3、命令下发</p>";
             eid.innerHTML = vb+vp;
             //layer.msg("TCP透传");
-        }else if (data.value == 6){
+        }else if (data.value == "RTMP"){
             var eid = document.getElementById("argument-details");
             var vb = "<b>RTMP协议基本功能介绍：</b>";
             var vp = "<p>1、rtmp实时流媒体推送</p>";
@@ -62,9 +63,13 @@ layui.define(['layer','form','element'],function (exports) {
         return false;
     });
     
-    form.on('submit(formDemo)', function (data) {
+    form.on('submit(ok)', function (data) {
+        //var prodname = document.getElementsByName("prodname");
+
+        //layer.msg(JSON.stringify(data.field));
+
         $.ajax({
-            url:"http://localhost:8080/login.do",
+            url:"http://localhost:8080/product",
             type:"post",
             contentType:"application/json",
             datatype:"json",
@@ -82,6 +87,7 @@ layui.define(['layer','form','element'],function (exports) {
                 layer.msg("error");
             }
         });
+        return false;
     });
 
     form.on('submit(cancel)', function (data) {
