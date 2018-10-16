3f7c6810d87a03e61c4cd393a27f1e39bbf37732
xiaodj <xiaodj20140808@163.com>
2018-10-17 02:04:52 +0800

    修改前端JS

--- DJWeb/src/main/webapp/js/manage.js
+++ DJWeb/src/main/webapp/js/manage.js
@@ -10,17 +10,18 @@ layui.define(['layer','element'],function (exports) {
     prodInstrModle();
     element.on('nav(manage)', function (data) {
         if (data.text() == "产品概况"){
-            prodInstrModle();
+
+            prodInstrModle(data);
         }else if (data.text() == "设备管理"){
-            devMng();
+            devMng(data);
         }else if (data.text() == "数据流模板"){
-            dataMoudle();
+            dataMoudle(data);
         }else if (data.text() == "设备群组"){
             document.getElementById("prodmanage").innerHTML="4";
-        }else if (data.text() == "APIKey管理"){
-            ApiMng();
+        }else if (data.text() == "权限管理"){
+            ApiMng(data);
         }else if (data.text() == "触发器管理"){
-            TriggeMng();
+            TriggeMng(data);
         }else if (data.text() == "应用管理"){
             document.getElementById("prodmanage").innerHTML="7";
         }else if (data.text() == "第三方开发平台"){
@@ -31,14 +32,14 @@ layui.define(['layer','element'],function (exports) {
     });
 
     //添加表格
-    function addTable(param){
+    function addTable(head, body){
         var tbEnm = document.createElement("table");
         tbEnm.className = "layui-table";
         //设置每列的范围
         var colgEnm = document.createElement("colgroup");
-        for (var i = 0; i < 4; i++){
+        for (var i = 0; i < head.count; i++){
             var colEnm = document.createElement("col");
-            colEnm.setAttribute("width", "25%");
+            colEnm.setAttribute("width", head.width[i].wd);
             colgEnm.appendChild(colEnm);
         }
         tbEnm.appendChild(colgEnm);
@@ -46,50 +47,25 @@ layui.define(['layer','element'],function (exports) {
         //设置每列的表头内容
         var thdEnm = document.createElement("thead");
         var trEnm = document.createElement("tr");
-        var thEnm = document.createElement("th");
-        var thText = document.createTextNode("产品ID");
-        thEnm.appendChild(thText);
-        trEnm.appendChild(thEnm);
-        thEnm = document.createElement("th");
-        thText = document.createTextNode("创建时间");
-        thEnm.appendChild(thText);
-        trEnm.appendChild(thEnm);
-        thEnm = document.createElement("th");
-        thText = document.createTextNode("API KEY");
-        thEnm.appendChild(thText);
-        trEnm.appendChild(thEnm);
-        thEnm = document.createElement("th");
-        thText = document.createTextNode("用户ID");
-        thEnm.appendChild(thText);
-        trEnm.appendChild(thEnm);
+        for (var i = 0; i < head.count; i++){
+            var thEnm = document.createElement("th");
+            var thText = document.createTextNode(head.title[i].tt);
+            thEnm.appendChild(thText);
+            trEnm.appendChild(thEnm);
+        }
         thdEnm.appendChild(trEnm);
         tbEnm.appendChild(thdEnm);
 
         //设置表的内容
         var tbdEnm = document.createElement("tbody");
-        for (var i = 0; i < 1; i++){
+        for (var i = 0; i < body.count; i++){
             var trEmn = document.createElement("tr");
-
-            var tdEnm = document.createElement("td");
-            var tdText = document.createTextNode("118990");
-            tdEnm.appendChild(tdText);
-            trEmn.appendChild(tdEnm);
-
-            tdEnm = document.createElement("td");
-            tdText = document.createTextNode("2018-01-25 15:22:38");
-            tdEnm.appendChild(tdText);
-            trEmn.appendChild(tdEnm);
-
-            tdEnm = document.createElement("td");
-            tdText = document.createTextNode("xJrWiFxr6LeO=TrvshVknO6=ZU8=");
-            tdEnm.appendChild(tdText);
-            trEmn.appendChild(tdEnm);
-
-            tdEnm = document.createElement("td");
-            tdText = document.createTextNode("60717");
-            tdEnm.appendChild(tdText);
-            trEmn.appendChild(tdEnm);
-
+            for (var j = 0; j < head.count; j++){
+                var tdEnm = document.createElement("td");
+                var tdText = document.createTextNode(body.data[i][j].dt);
+                tdEnm.appendChild(tdText);
+                trEmn.appendChild(tdEnm);
+            }
             tbdEnm.appendChild(trEmn);
         }
         tbEnm.appendChild(tbdEnm);
@@ -97,7 +73,7 @@ layui.define(['layer','element'],function (exports) {
     }
 
     //产品概要模块
-    function prodInstrModle() {
+    function prodInstrModle(data) {
         prodmnage.innerHTML = "";
         var fldEnm = document.createElement("fieldset");
         fldEnm.className = "layui-elem-field layui-field-title";
@@ -119,28 +95,37 @@ layui.define(['layer','element'],function (exports) {
         var divbdEnm = document.createElement("div");
         divbdEnm.className = "layui-card-body";
 
-        var strparam = '{\n' +
-            '"elmcount":4,\n' +
-            '"vlcount":1,\n' +
-            '"width":{\n' +
-            '"wd0":"25%",\n' +
-            '"wd1":"25%",\n' +
-            '"wd2":"25%",\n' +
-            '"wd3":"25%"\n' +
-            '},\n' +
-            '"name":{\n' +
-            '"nm0":"产品ID",\n' +
-            '"nm1":"创建时间",\n' +
-            '"nm2":"API KEY",\n' +
-            '"nm3":"用户ID"\n' +
-            '},\n' +
-            '"value":[\n' +
-            '{"vl0":"118990","vl1":"2018-01-25 15:22:38","vl2":"xJrWiFxr6LeO=TrvshVknO6=ZU8=","vl3":"60717"}\n' +
-            ']\n' +
-            '}';
-        var param = JSON.parse(strparam);
+        var shead = "{\n" +
+            "\t\"count\":4,\n" +
+            "\t\"width\":[\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"}\n" +
+            "\t],\n" +
+            "\t\"title\":[\n" +
+            "\t\t{\"tt\":\"产品ID\"},\n" +
+            "\t\t{\"tt\":\"创建时间\"},\n" +
+            "\t\t{\"tt\":\"API KEY\"},\n" +
+            "\t\t{\"tt\":\"用户ID\"}\n" +
+            "\t]\n" +
+            "}";
+
+        var head = JSON.parse(shead);
+        var vl0 = 1;
+        var vl1 = "2";
+        var vl2 = "1";
+        var vl3 = "2";
+        var vl4 = "1";
+        var sbody = "{\n" +
+            "\t\"count\":" + vl0 +",\n" +
+            "\t\"data\":[\n" +
+            "\t\t[{\"dt\":\""+ vl1 +"\"}, {\"dt\":\""+ vl2 +"\"},{\"dt\":\""+ vl3 +"\"}, {\"dt\":\""+ vl4 +"\"}]\n" +
+            "\t]\n" +
+            "}";
+        var body = JSON.parse(sbody);
         //添加一个表格
-        var tbEnm = addTable(param);
+        var tbEnm = addTable(head, body);
 
         divbdEnm.appendChild(tbEnm);
         divcdEnm.appendChild(divbdEnm);
@@ -150,7 +135,7 @@ layui.define(['layer','element'],function (exports) {
     }
 
     //设备管理
-    function devMng() {
+    function devMng(data) {
         prodmnage.innerHTML = "";
         var fldEnm = document.createElement("fieldset");
         fldEnm.className = "layui-elem-field layui-field-title";
@@ -166,34 +151,30 @@ layui.define(['layer','element'],function (exports) {
         divcdEnm.className = "layui-card";
         var divhdEnm = document.createElement("div");
         divhdEnm.className = "layui-card-header";
-        var divhdText = document.createTextNode("智能机器人");
+        var divhdText = document.createTextNode("设备数量：1个");
         divhdEnm.appendChild(divhdText);
         divcdEnm.appendChild(divhdEnm);
         var divbdEnm = document.createElement("div");
         divbdEnm.className = "layui-card-body";
 
-        var strparam = '{\n' +
-            '"elmcount":4,\n' +
-            '"vlcount":1,\n' +
-            '"width":{\n' +
-            '"wd0":"25%",\n' +
-            '"wd1":"25%",\n' +
-            '"wd2":"25%",\n' +
-            '"wd3":"25%"\n' +
-            '},\n' +
-            '"name":{\n' +
-            '"nm0":"产品ID",\n' +
-            '"nm1":"创建时间",\n' +
-            '"nm2":"API KEY",\n' +
-            '"nm3":"用户ID"\n' +
-            '},\n' +
-            '"value":[\n' +
-            '{"vl0":"118990","vl1":"2018-01-25 15:22:38","vl2":"xJrWiFxr6LeO=TrvshVknO6=ZU8=","vl3":"60717"}\n' +
-            ']\n' +
-            '}';
-        var param = JSON.parse(strparam);
+        var sParam = "{\n" +
+            "\t\"count\":4,\n" +
+            "\t\"width\":[\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"}\n" +
+            "\t],\n" +
+            "\t\"title\":[\n" +
+            "\t\t{\"tt\":\"设备名称\"},\n" +
+            "\t\t{\"tt\":\"设备ID\"},\n" +
+            "\t\t{\"tt\":\"数据保密性\"},\n" +
+            "\t\t{\"tt\":\"操作\"}\n" +
+            "\t]\n" +
+            "}";
+        var param = JSON.parse(sParam);
         //添加一个表格
-        var tbEnm = addTable(param);
+        var tbEnm = addTable(param, data);
 
         divbdEnm.appendChild(tbEnm);
         divcdEnm.appendChild(divbdEnm);
@@ -203,7 +184,7 @@ layui.define(['layer','element'],function (exports) {
     }
 
     //数据流摸版
-    function dataMoudle() {
+    function dataMoudle(data) {
         prodmnage.innerHTML = "";
         var fldEnm = document.createElement("fieldset");
         fldEnm.className = "layui-elem-field layui-field-title";
@@ -219,34 +200,30 @@ layui.define(['layer','element'],function (exports) {
         divcdEnm.className = "layui-card";
         var divhdEnm = document.createElement("div");
         divhdEnm.className = "layui-card-header";
-        var divhdText = document.createTextNode("智能机器人");
+        var divhdText = document.createTextNode("数据流展示");
         divhdEnm.appendChild(divhdText);
         divcdEnm.appendChild(divhdEnm);
         var divbdEnm = document.createElement("div");
         divbdEnm.className = "layui-card-body";
 
-        var strparam = '{\n' +
-            '"elmcount":4,\n' +
-            '"vlcount":1,\n' +
-            '"width":{\n' +
-            '"wd0":"25%",\n' +
-            '"wd1":"25%",\n' +
-            '"wd2":"25%",\n' +
-            '"wd3":"25%"\n' +
-            '},\n' +
-            '"name":{\n' +
-            '"nm0":"产品ID",\n' +
-            '"nm1":"创建时间",\n' +
-            '"nm2":"API KEY",\n' +
-            '"nm3":"用户ID"\n' +
-            '},\n' +
-            '"value":[\n' +
-            '{"vl0":"118990","vl1":"2018-01-25 15:22:38","vl2":"xJrWiFxr6LeO=TrvshVknO6=ZU8=","vl3":"60717"}\n' +
-            ']\n' +
-            '}';
-        var param = JSON.parse(strparam);
+        var sParam = "{\n" +
+            "\t\"count\":4,\n" +
+            "\t\"width\":[\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"}\n" +
+            "\t],\n" +
+            "\t\"title\":[\n" +
+            "\t\t{\"tt\":\"序号\"},\n" +
+            "\t\t{\"tt\":\"数据流名称\"},\n" +
+            "\t\t{\"tt\":\"单位符号\"},\n" +
+            "\t\t{\"tt\":\"操作\"}\n" +
+            "\t]\n" +
+            "}";
+        var param = JSON.parse(sParam);
         //添加一个表格
-        var tbEnm = addTable(param);
+        var tbEnm = addTable(param, data);
 
         divbdEnm.appendChild(tbEnm);
         divcdEnm.appendChild(divbdEnm);
@@ -255,13 +232,13 @@ layui.define(['layer','element'],function (exports) {
         prodmnage.appendChild(divEnm);
     }
 
-    //API 管理
-    function ApiMng() {
+    //权限管理
+    function ApiMng(data) {
         prodmnage.innerHTML = "";
         var fldEnm = document.createElement("fieldset");
         fldEnm.className = "layui-elem-field layui-field-title";
         var legEnm = document.createElement("legend");
-        var legText = document.createTextNode("API 管理");
+        var legText = document.createTextNode("权限管理");
         legEnm.appendChild(legText);
         fldEnm.appendChild(legEnm);
         prodmnage.appendChild(fldEnm);
@@ -272,34 +249,30 @@ layui.define(['layer','element'],function (exports) {
         divcdEnm.className = "layui-card";
         var divhdEnm = document.createElement("div");
         divhdEnm.className = "layui-card-header";
-        var divhdText = document.createTextNode("智能机器人");
+        var divhdText = document.createTextNode("API KEY数量：1个");
         divhdEnm.appendChild(divhdText);
         divcdEnm.appendChild(divhdEnm);
         var divbdEnm = document.createElement("div");
         divbdEnm.className = "layui-card-body";
 
-        var strparam = '{\n' +
-            '"elmcount":4,\n' +
-            '"vlcount":1,\n' +
-            '"width":{\n' +
-            '"wd0":"25%",\n' +
-            '"wd1":"25%",\n' +
-            '"wd2":"25%",\n' +
-            '"wd3":"25%"\n' +
-            '},\n' +
-            '"name":{\n' +
-            '"nm0":"产品ID",\n' +
-            '"nm1":"创建时间",\n' +
-            '"nm2":"API KEY",\n' +
-            '"nm3":"用户ID"\n' +
-            '},\n' +
-            '"value":[\n' +
-            '{"vl0":"118990","vl1":"2018-01-25 15:22:38","vl2":"xJrWiFxr6LeO=TrvshVknO6=ZU8=","vl3":"60717"}\n' +
-            ']\n' +
-            '}';
-        var param = JSON.parse(strparam);
+        var sParam = "{\n" +
+            "\t\"count\":4,\n" +
+            "\t\"width\":[\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"}\n" +
+            "\t],\n" +
+            "\t\"title\":[\n" +
+            "\t\t{\"tt\":\"名称\"},\n" +
+            "\t\t{\"tt\":\"APKey\"},\n" +
+            "\t\t{\"tt\":\"关联设备\"},\n" +
+            "\t\t{\"tt\":\"操作\"}\n" +
+            "\t]\n" +
+            "}";
+        var param = JSON.parse(sParam);
         //添加一个表格
-        var tbEnm = addTable(param);
+        var tbEnm = addTable(param, data);
 
         divbdEnm.appendChild(tbEnm);
         divcdEnm.appendChild(divbdEnm);
@@ -309,7 +282,7 @@ layui.define(['layer','element'],function (exports) {
     }
 
     //触发器管理
-    function TriggeMng(){
+    function TriggeMng(data){
         prodmnage.innerHTML = "";
         var fldEnm = document.createElement("fieldset");
         fldEnm.className = "layui-elem-field layui-field-title";
@@ -325,34 +298,30 @@ layui.define(['layer','element'],function (exports) {
         divcdEnm.className = "layui-card";
         var divhdEnm = document.createElement("div");
         divhdEnm.className = "layui-card-header";
-        var divhdText = document.createTextNode("智能机器人");
+        var divhdText = document.createTextNode("触发器数量：2个");
         divhdEnm.appendChild(divhdText);
         divcdEnm.appendChild(divhdEnm);
         var divbdEnm = document.createElement("div");
         divbdEnm.className = "layui-card-body";
 
-        var strparam = '{\n' +
-            '"elmcount":4,\n' +
-            '"vlcount":1,\n' +
-            '"width":{\n' +
-            '"wd0":"25%",\n' +
-            '"wd1":"25%",\n' +
-            '"wd2":"25%",\n' +
-            '"wd3":"25%"\n' +
-            '},\n' +
-            '"name":{\n' +
-            '"nm0":"产品ID",\n' +
-            '"nm1":"创建时间",\n' +
-            '"nm2":"API KEY",\n' +
-            '"nm3":"用户ID"\n' +
-            '},\n' +
-            '"value":[\n' +
-            '{"vl0":"118990","vl1":"2018-01-25 15:22:38","vl2":"xJrWiFxr6LeO=TrvshVknO6=ZU8=","vl3":"60717"}\n' +
-            ']\n' +
-            '}';
-        var param = JSON.parse(strparam);
+        var sParam = "{\n" +
+            "\t\"count\":4,\n" +
+            "\t\"width\":[\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"},\n" +
+            "\t\t{\"wd\":\"%25\"}\n" +
+            "\t],\n" +
+            "\t\"title\":[\n" +
+            "\t\t{\"tt\":\"触发器名称\"},\n" +
+            "\t\t{\"tt\":\"触发条件\"},\n" +
+            "\t\t{\"tt\":\"触发器信息接受方式\"},\n" +
+            "\t\t{\"tt\":\"操作\"}\n" +
+            "\t]\n" +
+            "}";
+        var param = JSON.parse(sParam);
         //添加一个表格
-        var tbEnm = addTable(param);
+        var tbEnm = addTable(param, data);
 
         divbdEnm.appendChild(tbEnm);
         divcdEnm.appendChild(divbdEnm);
